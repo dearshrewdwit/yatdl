be296940a13c1ed083cb561a4ba7ba61dd515208
Edward Withers <dearshrewdwit@gmail.com>
2020-09-09 18:13:52 +0100

    simple to do list

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app', () => {
   render(<App />);
+  const element = screen.getByText(/Yet Another To Do List/);
+  const textBox = screen.getByRole("textbox");
+  expect(element).toBeInTheDocument();
+  expect(textBox).toBeInTheDocument();
 });
---
+++ src/ToDoList.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+
+import ToDoList from './ToDoList';
+
+test('renders a to do list', () => {
+  render(<ToDoList />);
+  const element = screen.getByText(/Yet Another To Do List/);
+  const textBox = screen.getByRole("textbox");
+  expect(element).toBeInTheDocument();
+  expect(textBox).toBeInTheDocument();
+});
+
+test('user can add a todo', () => {
+  render(<ToDoList />);
+  const textBox = screen.getByRole("textbox");
+  userEvent.type(textBox, "A test todo!")
+  userEvent.click(screen.getByText('Submit'))
+  const todo = screen.getByText("A test todo!");
+  expect(todo).toBeInTheDocument();
+})
+
+test('user can check a todo', () => {
+  render(<ToDoList />);
+  const textBox = screen.getByRole("textbox");
+  userEvent.type(textBox, "A test todo!")
+  userEvent.click(screen.getByText('Submit'))
+  const checkbox = screen.getByRole("checkbox");
+  userEvent.click(checkbox)
+  expect(checkbox).toBeInTheDocument();
+})
+
+test('user can reset the list and remove the current todos', () => {
+  render(<ToDoList />);
+  const textBox = screen.getByRole("textbox");
+  userEvent.type(textBox, "A test todo!")
+  userEvent.click(screen.getByText('Submit'))
+  const resetLink = screen.getByText("reset");
+  userEvent.click(resetLink)
+  const todo = screen.queryByText("A test todo!");
+  expect(todo).not.toBeInTheDocument()
+})
